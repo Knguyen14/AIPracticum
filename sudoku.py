27810f6bb296fd788d6cd64eb8144f2744d44e39
SamBusser30 <sabu0818@colorado.edu>
2018-12-06 14:54:38 -0700

    Randomly generating sudoku board with random numbers, doesn't fit sudoku constraints yet though

--- sudoku.py
+++ sudoku.py
@@ -1,198 +1,52 @@
 import random
 #initialize board 
-sample_board = [[i for i in range(9)] for j in range(9)]
 
-#creating the board: need to randomly fill the 9x9 board, and then take some numbers away 
 
-#process: go through each cell and try to assign it a number, making sure it meets all constraints, 
-    
-#~ for i in range(9):
-    #~ for j in range(9):
-        #~ print(sample_board[i][j])
+board = [[i for i in range(9)] for j in range(9)]
 
 #prints board in grid form
-#~ for x in sample_board:
-    #~ print(*x, sep=" ")
+# for x in board:
+#     print(*x, sep=" ")
+
+def getBlocks(board):
+    answer = []
+    for r in range(3):
+        for c in range(3):
+            block = []
+            for i in range(3):
+                for j in range(3):
+                    block.append(board[3*r + i][3*c + j])
+            answer.append(block)
+    return answer
 
 
+#print(getBlocks(board))
+
+def assign_val(val, x, y,column):
+    #print(val, board[x][:], column)
+    if val not in board[x][:] and val not in column:
+        board[x][y] = num
+        return True
+    else:
+        return False
+
+for ii in range(0,9):
+    for jj in range(0,9):
+        column = []
+        for row in board:
+            column.append(row[jj])
+
+        board[ii][jj] = random.randint(0,9)
+        #print(row[jj], column)
+        #print(board[ii][:])
+        # okay = False
+        # while not okay:
+        #     num = random.randint(1, 10)
+        #     if assign_val(num, ii, jj, column) == True:
+        #         okay = True
 
 
-
-class cell():
-    """ Initilalizes cell object. A cell is a single box of a sudoku puzzle. 81 cells make up the body of a
-        sudoku puzzle. Initializes puzzle with all possible answers available, solved to false, and position of cell within the
-        sudoku puzzle"""
-    def __init__(self, position):
-        self.possibleAnswers = [1,2,3,4,5,6,7,8,9]
-        self.answer = None
-        self.position = position
-        self.solved = False
-        
-    def remove(self, num):
-        """Removes num from list of possible anwers in cell object."""
-        if num in self.possibleAnswers and self.solved == False:
-            self.possibleAnswers.remove(num)
-            if len(self.possibleAnswers) == 1:
-                self.answer = self.possibleAnswers[0]
-                self.solved = True
-        if num in self.possibleAnswers and self.solved == True:
-            self.answer = 0
-
-    def solvedMethod(self):
-        """ Returns whether or not a cell has been solved"""
-        return self.solved
-
-    def checkPosition(self):
-        """ Returns the position of a cell within a sudoku puzzle. x = row; y = col; z = box number"""
-        return self.position
-
-    def returnPossible(self):
-        """ Returns a list of possible answers that a cell can still use"""
-        return self.possibleAnswers
-
-    def lenOfPossible(self):
-        """ Returns an integer of the length of the possible answers list"""
-        return len(self.possibleAnswers)
-
-    def returnSolved(self):
-        """ Returns whether or not a cell has been solved"""
-        if self.solved == True:
-            return self.possibleAnswers[0]
-        else:
-            return 0
-        
-    def setAnswer(self, num):
-        """ Sets an answer of a puzzle and sets a cell's solved method to true. This
-            method also eliminates all other possible numbers"""
-        if num in [1,2,3,4,5,6,7,8,9]:
-            self.solved = True
-            self.answer = num
-            self.possibleAnswers = [num]
-        else:
-            raise(ValueError)
-       
-    def reset(self):
-        """ Resets all attributes of a cell to the original conditions""" 
-        self.possibleAnswers = [1,2,3,4,5,6,7,8,9]
-        self.answer = None
-        self.solved = False
-
-def emptySudoku():
-    ''' Creates an empty sudoku in row major form. Sets up all of the x, y, and z
-        coordinates for the sudoku cells'''
-    ans = []
-    for x in range(1,10):
-        if x in [7,8,9]:
-            intz = 7
-            z = 7
-        if x in [4,5,6]:
-            intz = 4
-            z = 4
-        if x in [1,2,3]:
-            intz = 1
-            z = 1
-        for y in range(1,10):
-            z = intz
-            if y in [7,8,9]:
-                z += 2
-            if y in [4,5,6]:
-                z += 1
-            if y in [1,2,3]:
-                z += 0
-            c = cell((x,y,z))
-            ans.append(c)
-    return ans
-
-
-
-def sudokuGen():
-    '''Generates a completed sudoku. Sudoku is completly random'''
-    cells = [i for i in range(81)] ## our cells is the positions of cells not currently set
-    sudoku = emptySudoku()
-    while len(cells) != 0:
-        lowestNum = []
-        Lowest = []
-        for i in cells:
-            lowestNum.append(sudoku[i].lenOfPossible())  ## finds all the lengths of of possible answers for each remaining cell
-        m = min(lowestNum)  ## finds the minimum of those
-        '''Puts all of the cells with the lowest number of possible answers in a list titled Lowest'''
-        for i in cells:
-            if sudoku[i].lenOfPossible() == m:
-                Lowest.append(sudoku[i])
-        '''Now we randomly choose a possible answer and set it to the cell'''
-        choiceElement = random.choice(Lowest)
-        choiceIndex = sudoku.index(choiceElement) 
-        cells.remove(choiceIndex)                 
-        position1 = choiceElement.checkPosition()
-        if choiceElement.solvedMethod() == False:  ##the actual setting of the cell
-            possibleValues = choiceElement.returnPossible()
-            finalValue = random.choice(possibleValues)
-            choiceElement.setAnswer(finalValue)
-            for i in cells:  ##now we iterate through the remaining unset cells and remove the input if it's in the same row, col, or box
-                position2 = sudoku[i].checkPosition()
-                if position1[0] == position2[0]:
-                    sudoku[i].remove(finalValue)
-                if position1[1] == position2[1]:
-                    sudoku[i].remove(finalValue)
-                if position1[2] == position2[2]:
-                    sudoku[i].remove(finalValue)
-
-        else:
-            finalValue = choiceElement.returnSolved()
-            for i in cells:  ##now we iterate through the remaining unset cells and remove the input if it's in the same row, col, or box
-                position2 = sudoku[i].checkPosition()
-                if position1[0] == position2[0]:
-                    sudoku[i].remove(finalValue)
-                if position1[1] == position2[1]:
-                    sudoku[i].remove(finalValue)
-                if position1[2] == position2[2]:
-                    sudoku[i].remove(finalValue)
-    return sudoku
-
-
-def printSudoku(sudoku):
-    '''Prints out a sudoku in a format that is easy for a human to read'''
-    row1 = []
-    row2 = []
-    row3 = []
-    row4 = []
-    row5 = []
-    row6 = []
-    row7 = []
-    row8 = []
-    row9 = []
-    for i in range(81):
-        if i in range(0,9):
-            row1.append(sudoku[i].returnSolved())
-        if i in range(9,18):
-            row2.append(sudoku[i].returnSolved())
-        if i in range(18,27):
-            row3.append(sudoku[i].returnSolved())
-        if i in range(27,36):
-            row4.append(sudoku[i].returnSolved())
-        if i in range(36,45):
-            row5.append(sudoku[i].returnSolved())
-        if i in range(45,54):
-            row6.append(sudoku[i].returnSolved())
-        if i in range(54,63):
-            row7.append(sudoku[i].returnSolved())
-        if i in range(63,72):
-            row8.append(sudoku[i].returnSolved())
-        if i in range(72,81):
-            row9.append(sudoku[i].returnSolved())
-    print(row1[0:3],row1[3:6],row1[6:10])
-    print(row2[0:3],row2[3:6],row2[6:10])
-    print(row3[0:3],row3[3:6],row3[6:10])
-    print('')
-    print(row4[0:3],row4[3:6],row4[6:10])
-    print(row5[0:3],row5[3:6],row5[6:10])
-    print(row6[0:3],row6[3:6],row6[6:10])
-    print('')
-    print(row7[0:3],row7[3:6],row7[6:10])
-    print(row8[0:3],row8[3:6],row8[6:10])
-    print(row9[0:3],row9[3:6],row9[6:10])
-    
-    
-game = sudokuGen()
-printSudoku(game)
-print(game[0][0])
+        #if num not in
+#prints board in grid form
+for x in board:
+    print(*x, sep=" ")
