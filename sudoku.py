8038183323b2007ffc6e2acde364d5a63a3570c1
SamBusser30 <sabu0818@colorado.edu>
2018-12-08 16:30:27 -0700

    generating a board now

--- sudoku.py
+++ sudoku.py
@@ -7,20 +7,32 @@ def cross(str_a, str_b):
 
 all_rows = 'ABCDEFGHI'
 all_cols = '123456789'
+rows = {'A': [], 'B':[], 'C':[], 'D':[], 'E':[], 'F':[], 'G':[], 'H':[], 'I':[]}
+cols = {'1':[], '2':[], '3':[], '4':[], '5':[], '6':[], '7':[], '8':[],'9':[]}
 
 # Build up list of all cell positions on the grid
 cells = cross(all_rows, all_cols)
 values = {}
 for cell in cells:
-    values[cell] = 0
+	values[cell] = 0
     
-for cell in cells:
-    row = cell[0]
-    column = cell[1]
-    
-    #print(row, column)
-    values[cell] = str(random.randint(1,9))
-    
+#~ for cell in cells:
+	#~ row = cell[0]
+	#~ column = cell[1]
+	#~ #want to check row
+	#~ good = False
+	#~ while good == False:
+		#~ num = str(random.randint(1,9))
+		#~ if num not in rows[row] and num not in cols[column]:
+			#~ good = True
+			#~ values[cell] = str(num)
+			#~ rows[row].append(num)
+			#~ cols[column].append(num)
+			#~ print(rows, cols)
+			
+	
+
+
 
 
 def display_grid(grid, coords=False):
@@ -81,4 +93,84 @@ def display_grid(grid, coords=False):
 	print(display)
 	return display
     
-display_grid(values)
+def sudoku(size):
+	import time
+	start_time=time.time()
+
+	import sys
+	import random as rn
+	mydict = {}
+	n = 0
+	print ('--started calculating--')
+	while len(mydict) < 9:
+		n += 1
+		x = range(1, size+1)
+		testlist = rn.sample(x, len(x))
+
+		isgood = True
+		for dictid,savedlist in mydict.items():
+			if isgood == False:
+				break
+			for v in savedlist:
+				if testlist[savedlist.index(v)] == v:
+					isgood = False
+					break
+
+		if isgood == True:
+			isgoodafterduplicatecheck = True
+			mod = len(mydict) % 3
+			dsavedlists = {}
+			dtestlists = {}
+			dcombindedlists = {}
+			for a in range(1,mod + 1):
+				savedlist = mydict[len(mydict) - a]               
+				for v1 in savedlist:
+					modsavedlists = (savedlist.index(v1) / 3) % 3
+					dsavedlists[len(dsavedlists)] = [modsavedlists,v1]
+				for t1 in testlist:
+					modtestlists = (testlist.index(t1) / 3) % 3
+					dtestlists[len(dtestlists)] = [modtestlists,t1]
+				for k,v2 in dsavedlists.items():
+					dcombindedlists[len(dcombindedlists)] = v2
+					dcombindedlists[len(dcombindedlists)] = dtestlists[k]
+			vsave = 0
+			lst1 = []
+			for k, vx in dcombindedlists.items():
+				vnew = vx[0]
+				if not vnew == vsave:
+					lst1 = []
+					lst1.append(vx[1])
+				else:
+					if vx[1] in lst1:
+						isgoodafterduplicatecheck = False
+						break
+					else:
+						lst1.append(vx[1])
+				vsave = vnew
+
+			if isgoodafterduplicatecheck == True:
+
+				mydict[len(mydict)] = testlist
+				print('success found', len(mydict), 'row')   
+
+	print ('--finished calculating--')
+	total_time = time.time()-start_time
+	return mydict, n, total_time
+
+return_dict, total_tries, amt_of_time = sudoku(9)
+print ('')
+print ('--printing output--')
+for n,v in return_dict.items():
+	print (n,v)
+print ('process took',total_tries,'tries in', round(amt_of_time,2), 'secs')
+print ('-------------------')
+
+#~ cell_index = 0
+#~ for entry in return_dict:
+	#~ for ind in range(len(return_dict[entry])):
+		#~ values[cells[cell_index]] = str(return_dict[entry][ind])
+			
+#~ display_grid(values)
+
+
+
